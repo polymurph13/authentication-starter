a56552617cd432533d3c85bcc5d3d5ce133d6bf1
Manish Poduval <manishpoduval@hotmail.com>
2020-06-02 09:33:03 +0530

    Error handling

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const createError = require('http-errors');
+
+//A library that helps us log the requests in the console
 const logger = require('morgan');
+
+// Used to setthe favicon for our app
 const favicon = require('serve-favicon');
 
 const cookieParser = require('cookie-parser');
@@ -21,33 +24,22 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+// setting up the middleware to let it know where to find the favicon icon
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+// Sets up morgan in our middleware so that we can see the requests getting logged
 app.use(logger('dev'));
+
+// a body parser to allow us to parse form submissions
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// helps us use the cookies from each request
 app.use(cookieParser());
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 // Routes middleware
 app.use('/', indexRouter);
-
-// Catch missing routes and forward to error handler
-app.use((req, res, next) => next(createError(404)));
-
-// Catch all error handler
-app.use((error, req, res) => {
-  // Set error information, with stack only available in development
-  res.locals.message = error.message;
-  res.locals.error = req.app.get('env') === 'development' ? error : {};
-
-  // render the error page
-  res.status(error.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,4 +4,7 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res) => res.render('index', { title: 'Welcome Ironhacker 🚀' }));
 
+
+router.get('*', (req, res) => res.render('error', { title: 'Page not found. Blame our developer. . .' }));
+
 module.exports = router;
